f8c89535655987401af812caddbc8c2a5db6fa00
zhangyuang <leonin0829@gmail.com>
2020-07-01 16:01:34 +0800

    chore: add inorder_traversal

--- README.md
+++ README.md
@@ -92,13 +92,14 @@ let next_node = node.next.take();
 
 #### 解题思路
 
-Rust 构造树需要使用 Rc<T>引用计数智能指针以及 RefCell，使得一个数据具有多个可变的所有者。因为一个子节点可能被多个父节点所共享。
+Rust 构造树需要使用 Rc<T>[引用计数智能指针](https://kaisery.github.io/trpl-zh-cn/ch15-04-rc.html)以及 [RefCell](https://kaisery.github.io/trpl-zh-cn/ch15-05-interior-mutability.html)，使得一个数据具有多个可变的所有者。因为一个子节点可能被多个父节点所共享。
 
 #### Medium
 
 中等难度的树题
 
-[二叉树前序遍历|preorder_traversal](./tree/medium/preorder_traversal/src/lib.rs)
+[二叉树前序遍历|preorder_traversal](./tree/medium/preorder_traversal/src/lib.rs)  
+[二叉树中序遍历|inorder_traversal](./tree/medium/inorder_traversal/src/lib.rs)
 
 ### Dynamic Programing
 
--- tree/medium/inorder_traversal/src/lib.rs
+++ tree/medium/inorder_traversal/src/lib.rs
@@ -27,7 +27,7 @@ impl TreeNode {
 use std::cell::RefCell;
 use std::rc::Rc;
 
-pub fn preorder_traversal(mut root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+pub fn inorder_traversal(mut root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
   let mut v = vec![];
   if root.is_none() {
     return v;
@@ -37,12 +37,12 @@ pub fn preorder_traversal(mut root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
     while !root.is_none() {
       // 一直添加左子树直到为空
       let node = root.unwrap();
-      v.push(node.borrow().val);
       root = node.borrow_mut().left.take();
       stack.push(node);
     }
     // 从栈中弹出，取节点的右子树
     root = stack.pop();
+    v.push(root.as_mut().unwrap().borrow().val);
     root = root.unwrap().borrow_mut().right.take();
   }
   v
