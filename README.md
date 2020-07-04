e63be8ed2557e82622d223d70b97340cf897fb12
zhangyuang <leonin0829@gmail.com>
2020-07-05 01:06:12 +0800

    chore: add binary_tree_paths

--- README.md
+++ README.md
@@ -7,14 +7,15 @@ Solve questions in [leetcode](https://leetcode-cn.com/) by Rust
 ## 前言
 
 由于 Rust 写数据结构相关的资料特别少并且理解非常困难，所以专门建了个 Repo 用来记录 Rust 刷 leetcode 的解法并包含心得体会，欢迎 Star✨ 会长期稳定更新。  
+努力写出最容易理解的 Rust 代码。
 [https://github.com/zhangyuang/leetcode](https://github.com/zhangyuang/leetcode)
 
 `注: 以下代码并没有刻意追求最优解，主要目的在于熟悉 Rust 语法以及使用可读性强便于理解的代码来解决问题。欢迎 Star✨ 长期稳定保持更新。`
 
 ## 相关资料
 
-官方 [API 文档](https://doc.rust-lang.org/std/index.html)  
-[Rust 程序设计语言中文版](https://kaisery.github.io/)
+[官方 API 文档](https://doc.rust-lang.org/std/index.html)  
+[Rust 程序设计语言中文版](https://kaisery.github.io/trpl-zh-cn/)
 
 ## 分类
 
@@ -102,7 +103,8 @@ Rust 构造树需要使用 [Rc<T>引用计数智能指针](https://kaisery.githu
 [二叉树的层平均值|average_of_levels](./tree/easy/average_of_levels/src/lib.rs)  
 [相同的树|is_symmetric](./tree/easy/is_symmetric/src/lib.rs)  
 [对称二叉树|is_same_tree](./tree/easy/is_same_tree/src/lib.rs)  
-[平衡二叉树|is_balanced](./tree/easy/is_balanced/src/lib.rs)
+[平衡二叉树|is_balanced](./tree/easy/is_balanced/src/lib.rs)  
+[二叉树的所有路径|binary_tree_paths](./tree/easy/binary_tree_paths/src/lib.rs)  
 
 #### Medium
 
--- tree/easy/binary_tree_paths/src/lib.rs
+++ tree/easy/binary_tree_paths/src/lib.rs
@@ -17,17 +17,52 @@ impl TreeNode {
   }
 }
 
+// 输入:
+
+//    1
+//  /   \
+// 2     3
+//  \
+//   5
+
+// 输出: ["1->2->5", "1->3"]
+
+// 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+
 use std::cell::RefCell;
 use std::rc::Rc;
 
 pub fn binary_tree_paths(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<String> {
-  let v = vec![];
-  find_leaf(root, &v);
+  let mut v = vec![];
+  find_leaf(&root, &mut v, None);
   v
 }
 
-fn find_leaf(root: Option<Rc<RefCell<TreeNode>>>, v: &Vec<String>) {
+fn find_leaf(root: &Option<Rc<RefCell<TreeNode>>>,  v: &mut Vec<String>, str: Option<&mut String>) {
+  // 递归找到叶子节点记录路径即可
   if root.is_none() {
+    return
+  }
+  let val = &root.as_ref().unwrap().borrow().val.to_string();
+  let left = &root.as_ref().unwrap().borrow().left;
+  let right = &root.as_ref().unwrap().borrow().right;
+
+  let mut res_str;
+  match str {
+    None => {
+      res_str = String::from("");
+    },
+    Some(str) => {
+      res_str = str.to_string();
+    }
+  }
+  res_str.push_str(val);
+  if left.is_none() && right.is_none() {
+    // 说明当前为叶子节点
+    v.push(res_str);
     return;
   }
+  res_str.push_str("->");
+  find_leaf(left, v, Some(&mut res_str));
+  find_leaf(right, v,Some(&mut res_str));
 }
