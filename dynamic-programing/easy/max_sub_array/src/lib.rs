8429547d40306f784e4ef8a4b3d353860d50564e
zhangyuang <leonin0829@gmail.com>
2020-07-08 01:20:37 +0800

    chore: update max_sub_array

--- dynamic-programing/easy/max_sub_array/src/lib.rs
+++ dynamic-programing/easy/max_sub_array/src/lib.rs
@@ -3,17 +3,17 @@ struct Solution {}
 use std::cmp::max;
 
 impl Solution {
-  pub fn max_sub_array(mut nums: Vec<i32>) -> i32 {
+  pub fn max_sub_array(nums: Vec<i32>) -> i32 {
     let mut res = nums[0];
-    if nums.len() == 1 {
-      return res;
-    }
-    let mut i = 1;
-    while i < nums.len() {
-      // 动态规划，nums[i]代表的含义为到当前第i个元素的最大子序列和
-      nums[i] = max(nums[i], nums[i] + nums[i - 1]);
-      res = max(res, nums[i]);
-      i += 1;
+    let mut sum = 0;
+    for num in nums {
+      if sum <= 0 {
+        sum = num;
+      } else {
+        // 如果当前的sum大于0就一直加下去。如果小于0就从当前值重新开始
+        sum += num;
+      }
+      res = max(res, sum);
     }
     res
   }
