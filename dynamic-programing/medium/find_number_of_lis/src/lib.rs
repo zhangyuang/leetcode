c275b40c3e334132ad66c4eee1ef4fc06abd8fb7
zhangyuang <leonin0829@gmail.com>
2020-07-28 22:31:15 +0800

    chore: update find_number_of_lis

--- dynamic-programing/medium/find_number_of_lis/src/lib.rs
+++ dynamic-programing/medium/find_number_of_lis/src/lib.rs
@@ -20,24 +20,21 @@ impl Solution {
     let mut max_len = 0;
     // dp[i]代表到i为止最长的递增子序列长度
     for i in 1..nums.len() {
-      if nums[i] > nums[i - 1] {
-        dp[i] = dp[i - 1] + 1;
-      } else {
-        // 到这里连续递增中止,从已匹配的vec中寻找当前的
-        println!("{:?}", nums[i]);
-        for j in 0..i {
-          if nums[i] < nums[j] {
-            dp[i] = dp[j - 1] + 1;
-            break;
-          }
+      for j in 0..i {
+        if nums[i] > nums[j] {
+          dp[i] = max(dp[i], dp[j] + 1)
         }
       }
       max_len = max(max_len, dp[i]);
     }
     let mut item = vec![dp[0]];
+    if max_len == 1 {
+      return dp.len() as i32;
+    }
     println!("{:?}", dp);
+
     Solution::find_path(&dp, &mut path, max_len as usize, &mut item);
-    // println!("{:?}", path);
+    println!("{:?}", path);
     path.len() as i32
   }
   fn find_path(dp: &Vec<i32>, path: &mut Vec<Vec<i32>>, max_len: usize, item: &mut Vec<i32>) {
@@ -63,6 +60,6 @@ mod tests {
 
   #[test]
   fn tests() {
-    Solution::find_number_of_lis(vec![1, 3, 1, 4, 7]);
+    Solution::find_number_of_lis(vec![1, 3, 5, 4, 7]);
   }
 }
