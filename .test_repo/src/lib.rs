b4befc31748b7e1f6aad36a0e4b6c362709d08b8
zhangyuang <leonin0829@gmail.com>
2020-07-30 22:09:34 +0800

    chore: update .test_repo

--- .test_repo/src/lib.rs
+++ .test_repo/src/lib.rs
@@ -1,55 +1,82 @@
 /*
- * @lc app=leetcode.cn id=3 lang=rust
+ * @lc app=leetcode.cn id=108 lang=rust
  *
- * [3] 无重复字符的最长子串
+ * [108] 将有序数组转换为二叉搜索树
  */
-pub struct Solution {}
 
-// @lc code=start
-use std::cmp::max;
-impl Solution {
-    pub fn length_of_longest_substring(s: String) -> i32 {
-        // 使用滑动窗口
-        if s == "" {
-            return 0;
+struct Solution {}
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
         }
-        let mut start = 0;
-        let mut end = 1;
-        let mut res = 1; // 获取当前窗口长度
-        while end != s.len() {
-            // println!("{}  {}", &s[start..end], &s[end..end + 1]);
-            if !&s[start..end].contains(&s[end..end + 1]) {
-                end += 1; // 窗口向右扩大
-                res = max(res, s[start..end].len())
-            } else {
-                // 窗口从左边缩小
-                start += 1;
-                if start >= end {
-                    end = start + 1;
-                }
-            }
-        }
-        println!("{}", res);
-        res as i32
     }
+}
+// @lc code=start
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+//
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn sorted_array_to_bst(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut root_index = 0;
+        let mut root = if nums.len() % 2 != 0 {
+            root_index = nums.len() / 2;
+            create_node(nums[root_index])
+        } else {
+            root_index = nums.len() / 2 - 1;
+            create_node(nums[root_index])
+        };
+        let mut node = root;
+        let mut node_clone = node.clone();
+        while root_index + 2 < nums.len() + 2 {
+            // 代表右边有两个数字
+            node.as_mut().unwrap().borrow_mut().right = create_node(nums[root_index + 2]);
+            node.as_mut()
+                .unwrap()
+                .borrow_mut()
+                .right
+                .as_mut()
+                .unwrap()
+                .borrow_mut()
+                .left = create_node(nums[root_index + 1]);
+            root_index = root_index + 2;
+            node = node.as_mut().unwrap().borrow_mut().right.clone();
+        }
+        println!("{:?}", node);
+        // if root_index == nums.len() - 2 {
+        //     node.as_mut().unwrap().borrow_mut().right = create_node(nums[root_index + 1])
+        // }
+        root
+    }
+}
+fn create_node(val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    Some(Rc::new(RefCell::new(TreeNode::new(val))))
 }
 // @lc code=end
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn tests() {
-        assert_eq!(
-            Solution::length_of_longest_substring("pwwkew".to_string()),
-            3
-        );
-        assert_eq!(Solution::length_of_longest_substring("aaa".to_string()), 1);
-
-        assert_eq!(Solution::length_of_longest_substring("dvdf".to_string()), 3);
-        assert_eq!(
-            Solution::length_of_longest_substring("anviaj".to_string()),
-            5
-        );
-    }
-}
