34a33c83dabff926d143312435ba7823a5c2273e
zhangyuang <leonin0829@gmail.com>
2020-12-08 17:15:02 +0800

    chore: add MinStack

--- .test_repo/src/lib.rs
+++ .test_repo/src/lib.rs
@@ -1,45 +1,74 @@
-/*
- * @lc app=leetcode.cn id=41 lang=rust
- *
- * [41] 缺失的第一个正数
- */
-struct Solution {}
-// @lc code=start
-use std::cmp::max;
+// 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
 
-impl Solution {
-    pub fn first_missing_positive(nums: Vec<i32>) -> i32 {
-        if nums.len() == 0 {
-            return 1;
-        }
-        let mut v: Vec<i32> = vec![-1; nums.len()];
-        let mut MAX = 0;
-        for i in nums {
-            if i > 0 && i < v.len() as i32 {
-                v[i as usize] = 1;
-            }
-            MAX = max(MAX, i)
-        }
-        for i in 1..v.len() {
-            if v[i] < 0 {
-                return i as i32;
-            }
-        }
-        return if v.len() >= MAX as usize {
-            MAX + 1
-        } else {
-            v.len() as i32
+//
+
+// 示例:
+
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.min();   --> 返回 -3.
+// minStack.pop();
+// minStack.top();      --> 返回 0.
+// minStack.min();   --> 返回 -2.
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+struct MinStack {
+    top: Option<i32>, // 栈顶指针
+    stack: Vec<i32>,
+    min_stack: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MinStack {
+    /** initialize your data structure here. */
+    fn new() -> Self {
+        return MinStack {
+            stack: vec![],
+            top: None,
+            min_stack: vec![],
         };
     }
-}
-// @lc code=end
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn tests() {
-        let nums: Vec<i32> = vec![2];
-        println!("{:?}", Solution::first_missing_positive(nums));
+    fn push(&mut self, x: i32) {
+        self.stack.push(x);
+        self.top = Some(x);
+        if self.min_stack.len() == 0 || x <= self.min_stack[self.min_stack.len() - 1] {
+            self.min_stack.push(x)
+        }
+    }
+
+    fn pop(&mut self) {
+        if self.stack.pop().unwrap() == self.min_stack[self.min_stack.len() - 1] {
+            self.min_stack.pop();
+        }
+        if self.stack.len() != 0 {
+            self.top = Some(self.stack[self.stack.len() - 1]); // 栈顶指针 -1
+        } else {
+            self.top = None
+        }
+    }
+
+    fn top(&self) -> i32 {
+        self.top.unwrap()
+    }
+
+    fn min(&self) -> i32 {
+        self.min_stack[self.min_stack.len() - 1]
     }
 }
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * let obj = MinStack::new();
+//  * obj.push(x);
+//  * obj.pop();
+//  * let ret_3: i32 = obj.top();
+//  * let ret_4: i32 = obj.min();
+//  */
---
+++ others/easy/MinStack/src/lib.rs
@@ -0,0 +1,74 @@
+// 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+
+//
+
+// 示例:
+
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.min();   --> 返回 -3.
+// minStack.pop();
+// minStack.top();      --> 返回 0.
+// minStack.min();   --> 返回 -2.
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/bao-han-minhan-shu-de-zhan-lcof
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+struct MinStack {
+  top: Option<i32>, // 栈顶指针
+  stack: Vec<i32>,
+  min_stack: Vec<i32>,
+}
+
+/**
+* `&self` means the method takes an immutable reference.
+* If you need a mutable reference, change it to `&mut self` instead.
+*/
+impl MinStack {
+  /** initialize your data structure here. */
+  fn new() -> Self {
+    return MinStack {
+      stack: vec![],
+      top: None,
+      min_stack: vec![],
+    };
+  }
+
+  fn push(&mut self, x: i32) {
+    self.stack.push(x);
+    self.top = Some(x);
+    if self.min_stack.len() == 0 || x <= self.min_stack[self.min_stack.len() - 1] {
+      self.min_stack.push(x)
+    }
+  }
+
+  fn pop(&mut self) {
+    if self.stack.pop().unwrap() == self.min_stack[self.min_stack.len() - 1] {
+      self.min_stack.pop();
+    }
+    if self.stack.len() != 0 {
+      self.top = Some(self.stack[self.stack.len() - 1]); // 栈顶指针 -1
+    } else {
+      self.top = None
+    }
+  }
+
+  fn top(&self) -> i32 {
+    self.top.unwrap()
+  }
+
+  fn min(&self) -> i32 {
+    self.min_stack[self.min_stack.len() - 1]
+  }
+}
+
+// /**
+//  * Your MinStack object will be instantiated and called as such:
+//  * let obj = MinStack::new();
+//  * obj.push(x);
+//  * obj.pop();
+//  * let ret_3: i32 = obj.top();
+//  * let ret_4: i32 = obj.min();
+//  */
