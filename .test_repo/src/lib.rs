76a8f45b3e5f032a1414cfad5e82915d7f0c6c84
zhangyuang <leonin0829@gmail.com>
2020-12-20 22:23:26 +0800

    chore: update balance_bst

--- .test_repo/src/lib.rs
+++ .test_repo/src/lib.rs
@@ -41,52 +41,65 @@ struct Solution {}
 //   }
 // }
 use std::cell::RefCell;
+use std::cmp::max;
 use std::rc::Rc;
+
 impl Solution {
-    // 解法1，中序遍历后构建 avl 树
-    // pub fn balance_bst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-    //     let in_order_traversal = Self::in_order_traversal(&root)?;
-    //     Self::build_balance_bst_from_arr(in_order_traversal)
-    // }
-    // fn build_balance_bst_from_arr(nums: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
-    //     if nums.len() == 0 {
-    //         return None;
-    //     }
-    //     let mut root_index = 0;
-    //     let mut root = if nums.len() % 2 != 0 {
-    //         root_index = nums.len() / 2;
-    //         Self::create_node(nums[root_index])
-    //     } else {
-    //         root_index = nums.len() / 2 - 1;
-    //         Self::create_node(nums[root_index])
-    //     };
-    //     root.as_mut()?.borrow_mut().right =
-    //         Self::build_balance_bst_from_arr(nums[root_index + 1..nums.len()].to_vec());
-    //     root.as_mut()?.borrow_mut().left =
-    //         Self::build_balance_bst_from_arr(nums[0..root_index].to_vec());
-    //     root
-    // }
-    // fn create_node(val: i32) -> Option<Rc<RefCell<TreeNode>>> {
-    //     Some(Rc::new(RefCell::new(TreeNode::new(val))))
-    // }
-    // fn in_order_traversal(root: &Option<Rc<RefCell<TreeNode>>>) -> Option<Vec<i32>> {
-    //     let mut v = vec![];
-    //     let mut stack = vec![];
-    //     let mut node = root.clone();
-    //     while stack.len() != 0 || node.is_some() {
-    //         while node.is_some() {
-    //             let res = node?;
-    //             node = res.borrow_mut().left.take();
-    //             stack.push(res);
-    //         }
-    //         node = stack.pop();
-    //         v.push(node.as_ref()?.borrow_mut().val);
-    //         node = node?.borrow_mut().right.take();
-    //     }
-    //     Some(v)
-    // }
-    pub fn balance_bst(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    fn create_node(val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        Some(Rc::new(RefCell::new(TreeNode::new(val))))
+    }
+    pub fn balance_bst(mut root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+        let mut new_root = None;
+        let mut stack = vec![];
+        while stack.len() != 0 || !root.is_none() {
+            // 先序遍历原树，构建新的 avl 树
+            while !root.is_none() {
+                // 一直添加左子树直到为空
+                let node = root.unwrap();
+                new_root = Self::insert(new_root, node.borrow().val);
+                root = node.borrow_mut().left.take();
+                stack.push(node);
+            }
+            // 从栈中弹出，取节点的右子树
+            root = stack.pop();
+            root = root.unwrap().borrow_mut().right.take();
+        }
+        new_root
+    }
+    fn insert(mut root: Option<Rc<RefCell<TreeNode>>>, val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+        if root.is_none() {
+            return Self::create_node(val);
+        }
+        let mut root_borrow_mut = root.as_mut()?.borrow_mut();
+        let left = root_borrow_mut.left.take();
+        let right = root_borrow_mut.right.take();
+        let root_val = root_borrow_mut.val;
+        if root_val > val {
+            root_borrow_mut.left = Self::insert(left, val)
+        } else {
+            root_borrow_mut.right = Self::insert(right, val)
+        }
         root
+    }
+    fn get_balance_factor(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        // 获取节点平衡因子
+        if root.is_none() {
+            return 0;
+        }
+        let root_borrow = root.as_ref().unwrap().borrow();
+        let left = &root_borrow.left;
+        let right = &root_borrow.right;
+        return (Self::get_height(left) - Self::get_height(right)).abs();
+    }
+    fn get_height(root: &Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        // 获取节点的高度
+        if root.is_none() {
+            return 0;
+        }
+        let root_borrow = root.as_ref().unwrap().borrow();
+        let left = &root_borrow.left;
+        let right = &root_borrow.right;
+        return max(Self::get_height(left), Self::get_height(right)) + 1;
     }
 }
 // @lc code=end
